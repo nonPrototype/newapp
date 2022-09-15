f77608144b1d4a45d85709a06e98ea0eb32dcf31
Camilo <j.camilo4gd@gmail.com>
2022-09-15 09:12:02 -0500

    add and configure jwt

---
+++ authApp/views/customers/create.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def create_customers(request):
+    return HttpResponse("CREACION DE CUSTOMER")
---
+++ authApp/views/trip/create.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def create_trip(request):
+    return HttpResponse("MODULO DE CREACION DE VIAJES")
